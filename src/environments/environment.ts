8867bfd4d8c4a6e5e449adf09aa1a39b094832ee
Alberto Acevedo <meyling.arias@contollo.net>
2023-03-11 11:42:51 -0600

    feat(environment): add environment variables to connect to the API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_BACKEND: 'web-production-e023.up.railway.app'
 };
 
 /*
